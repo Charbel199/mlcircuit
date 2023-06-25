d456affde6b835c2f7c9cfad2701be5e6983f0a7
Charbel199 <charbel-boumaroun@outlook.com>
2023-06-25 12:59:05 +0300

    Add setup and initial requirements

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+import os
+from setuptools import setup
+
+directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(name='mlcircuit',
+      version='0.0.1',
+      description='An end-to-end ML pipeline package.',
+      author='Charbel Bou Maroun',
+      license='MIT',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      packages=['mlcircuit', 'mlcircuit.framework', 'mlcircuit.model', 'mlcircuit.runtime'],
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: MIT License"
+      ],
+      install_requires=['numpy', 'requests', 'pillow', 'tqdm', 'networkx', 'pyopencl', 'PyYAML'],
+      python_requires='>=3.8',
+      extras_require={
+          'testing': [
+              "torch",
+              "tensorflow",
+              "pytest",
+              "pytest-xdist",
+              "onnx"
+          ],
+      },
+      include_package_data=True)
